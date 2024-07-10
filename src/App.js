c481f83532937b24a292867da76781871d672759
koqonut <9313346+koqonut@users.noreply.github.com>
2024-07-09 23:02:54 -0400

    create new react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { PRODUCTS } from './data/dataset';
+
+/**
+FilterableProductTable
+  SearchBar
+  ProductTable
+    ProductCategoryRow
+    ProductRow
+ */
+import { useState } from 'react';
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly} />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly} />
     </div>
   );
 }
 
-export default App;
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+      <td>{product.store}</td>
+      <td>{product.city}</td>
+      <td>{product.lastUpdatedDate}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products, filterText, inStockOnly }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (
+      product.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    ) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category} />
+      );
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    );
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Store</th>
+          <th>City</th>
+          <th>LastUpdatedDate</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange
+}) {
+  return (
+    <form>
+      <input
+        type="text"
+        value={filterText} placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)} />
+      <label>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)} />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
---
+++ src/data/dataset.js
@@ -0,0 +1,364 @@
+export const PRODUCTS = [
+    {
+        "category": "Fruits",
+        "price": "$3.11",
+        "stocked": true,
+        "name": "Grape",
+        "lastUpdatedDate": "2024-02-26",
+        "store": "Walmart",
+        "city": "Toronto"
+    },
+    {
+        "category": "Fruits",
+        "price": "$2.09",
+        "stocked": false,
+        "name": "Strawberry",
+        "lastUpdatedDate": "2023-07-04",
+        "store": "Walmart",
+        "city": "Toronto"
+    },
+    {
+        "category": "Fruits",
+        "price": "$3.43",
+        "stocked": true,
+        "name": "Mango",
+        "lastUpdatedDate": "2024-04-27",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+    {
+        "category": "Fruits",
+        "price": "$0.61",
+        "stocked": false,
+        "name": "Pineapple",
+        "lastUpdatedDate": "2023-08-03",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Fruits",
+        "price": "$1.52",
+        "stocked": false,
+        "name": "Banana",
+        "lastUpdatedDate": "2024-05-03",
+        "store": "Walmart",
+        "city": "Toronto"
+    },
+    {
+        "category": "Fruits",
+        "price": "$4.26",
+        "stocked": false,
+        "name": "Orange",
+        "lastUpdatedDate": "2023-11-06",
+        "store": "Kroger",
+        "city": "Brampton"
+    },
+    {
+        "category": "Fruits",
+        "price": "$4.63",
+        "stocked": true,
+        "name": "Blueberry",
+        "lastUpdatedDate": "2024-01-27",
+        "store": "Walmart",
+        "city": "Toronto"
+    },
+    {
+        "category": "Fruits",
+        "price": "$3.74",
+        "stocked": true,
+        "name": "Apple",
+        "lastUpdatedDate": "2023-12-26",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+    {
+        "category": "Fruits",
+        "price": "$0.67",
+        "stocked": false,
+        "name": "Watermelon",
+        "lastUpdatedDate": "2024-06-13",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+    {
+        "category": "Fruits",
+        "price": "$2.33",
+        "stocked": true,
+        "name": "Cherry",
+        "lastUpdatedDate": "2024-03-19",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$3.90",
+        "stocked": true,
+        "name": "Spinach",
+        "lastUpdatedDate": "2024-01-03",
+        "store": "Walmart",
+        "city": "Toronto"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$2.43",
+        "stocked": false,
+        "name": "Broccoli",
+        "lastUpdatedDate": "2023-10-10",
+        "store": "Kroger",
+        "city": "Brampton"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$3.12",
+        "stocked": false,
+        "name": "Tomato",
+        "lastUpdatedDate": "2024-05-23",
+        "store": "Whole Foods",
+        "city": "Ottawa"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$2.31",
+        "stocked": true,
+        "name": "Cucumber",
+        "lastUpdatedDate": "2023-11-03",
+        "store": "Whole Foods",
+        "city": "Ottawa"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$2.86",
+        "stocked": true,
+        "name": "Lettuce",
+        "lastUpdatedDate": "2023-11-07",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$1.55",
+        "stocked": true,
+        "name": "Garlic",
+        "lastUpdatedDate": "2023-02-27",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$2.55",
+        "stocked": true,
+        "name": "Potato",
+        "lastUpdatedDate": "2024-02-17",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$3.52",
+        "stocked": true,
+        "name": "Carrot",
+        "lastUpdatedDate": "2024-05-31",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$1.19",
+        "stocked": true,
+        "name": "Pepper",
+        "lastUpdatedDate": "2023-07-10",
+        "store": "Walmart",
+        "city": "Toronto"
+    },
+    {
+        "category": "Vegetables",
+        "price": "$2.20",
+        "stocked": false,
+        "name": "Onion",
+        "lastUpdatedDate": "2023-08-11",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+
+    {
+        "category": "Dairy",
+        "price": "$10.25",
+        "stocked": true,
+        "name": "Milk",
+        "lastUpdatedDate": "2023-03-03",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Dairy",
+        "price": "$17.00",
+        "stocked": true,
+        "name": "Cheese",
+        "lastUpdatedDate": "2023-04-13",
+        "store": "Kroger",
+        "city": "Brampton"
+    },
+    {
+        "category": "Dairy",
+        "price": "$17.12",
+        "stocked": false,
+        "name": "Yogurt",
+        "lastUpdatedDate": "2024-01-04",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Dairy",
+        "price": "$12.69",
+        "stocked": true,
+        "name": "Butter",
+        "lastUpdatedDate": "2023-01-31",
+        "store": "Kroger",
+        "city": "Brampton"
+    },
+    {
+        "category": "Dairy",
+        "price": "$18.20",
+        "stocked": false,
+        "name": "Cream",
+        "lastUpdatedDate": "2023-12-01",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+    {
+        "category": "Beverages",
+        "price": "$17.07",
+        "stocked": true,
+        "name": "Coffee",
+        "lastUpdatedDate": "2023-10-25",
+        "store": "Whole Foods",
+        "city": "Ottawa"
+    },
+    {
+        "category": "Beverages",
+        "price": "$7.16",
+        "stocked": false,
+        "name": "Tea",
+        "lastUpdatedDate": "2023-11-16",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+    {
+        "category": "Beverages",
+        "price": "$4.68",
+        "stocked": true,
+        "name": "Juice",
+        "lastUpdatedDate": "2023-09-13",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+    {
+        "category": "Beverages",
+        "price": "$4.04",
+        "stocked": true,
+        "name": "Soda",
+        "lastUpdatedDate": "2023-11-30",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Beverages",
+        "price": "$14.02",
+        "stocked": true,
+        "name": "Water",
+        "lastUpdatedDate": "2023-10-28",
+        "store": "Whole Foods",
+        "city": "Ottawa"
+    },
+    {
+        "category": "Snacks",
+        "price": "$1.53",
+        "stocked": true,
+        "name": "Chips",
+        "lastUpdatedDate": "2024-01-12",
+        "store": "Whole Foods",
+        "city": "Ottawa"
+    },
+    {
+        "category": "Snacks",
+        "price": "$16.83",
+        "stocked": false,
+        "name": "Chocolate",
+        "lastUpdatedDate": "2024-04-19",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Snacks",
+        "price": "$12.71",
+        "stocked": true,
+        "name": "Cookies",
+        "lastUpdatedDate": "2023-05-14",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Snacks",
+        "price": "$1.67",
+        "stocked": false,
+        "name": "Nuts",
+        "lastUpdatedDate": "2023-02-19",
+        "store": "Metro",
+        "city": "Mississauga"
+    },
+    {
+        "category": "Snacks",
+        "price": "$10.48",
+        "stocked": true,
+        "name": "Granola Bars",
+        "lastUpdatedDate": "2023-03-18",
+        "store": "Walmart",
+        "city": "Toronto"
+    },
+    {
+        "category": "Personal Care",
+        "price": "$3.00",
+        "stocked": true,
+        "name": "Shampoo",
+        "lastUpdatedDate": "2023-05-05",
+        "store": "Kroger",
+        "city": "Brampton"
+    },
+    {
+        "category": "Personal Care",
+        "price": "$16.85",
+        "stocked": true,
+        "name": "Soap",
+        "lastUpdatedDate": "2023-08-17",
+        "store": "Trader Joe's",
+        "city": "Hamilton"
+    },
+    {
+        "category": "Personal Care",
+        "price": "$5.87",
+        "stocked": true,
+        "name": "Toothpaste",
+        "lastUpdatedDate": "2023-02-21",
+        "store": "Kroger",
+        "city": "Brampton"
+    },
+    {
+        "category": "Personal Care",
+        "price": "$12.60",
+        "stocked": true,
+        "name": "Deodorant",
+        "lastUpdatedDate": "2023-03-06",
+        "store": "Whole Foods",
+        "city": "Ottawa"
+    },
+    {
+        "category": "Personal Care",
+        "price": "$2.17",
+        "stocked": false,
+        "name": "Lotion",
+        "lastUpdatedDate": "2023-12-29",
+        "store": "Metro",
+        "city": "Mississauga"
+    }
+
+]
