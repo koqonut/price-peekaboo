7fd162e999115d8b0911b3a2a19890d4faa87c08
koqonut <9313346+koqonut@users.noreply.github.com>
2024-07-15 20:20:28 -0400

    remove mobile specific header code

--- src/components/ResponsiveHeaderComponent.js
+++ src/components/ResponsiveHeaderComponent.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import useWindowSize from './useWindowSize';
-import { MOBILE_SCREEN_CUTOFF } from '../utils/Constants';
-
-const MobileHeader = () => (
-    <header className="mobile-header relative w-full">
-        <div className="header-overlay absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center">
-        </div>
-    </header>
-);
-
-const LaptopHeader = () => (
-    <header className="laptop-header relative w-full ">
-        <div className="header-overlay absolute inset-0 bg-black bg-opacity-100 flex flex-col justify-center items-center">
-        </div>
-    </header>
-);
 
 const ResponsiveHeader = () => {
-    const size = useWindowSize();
+    return (
+        <header className="laptop-header relative w-full ">
+            <div className="header-overlay absolute inset-0 bg-black bg-opacity-100 flex flex-col justify-center items-center">
+            </div>
+        </header>
+    );
 
-    return size.width < MOBILE_SCREEN_CUTOFF ? <MobileHeader /> : <LaptopHeader />;
 };
-
 export default ResponsiveHeader;
